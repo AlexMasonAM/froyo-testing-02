c881a6c8c5ba6a51f2f43fb171223317de2e78f4
AlexMasonAM <alexmmason@gmail.com>
2015-02-04 10:16:49 -0800

    added RSpec tests for Yogurt instances

--- app/models/yogurt.rb
+++ app/models/yogurt.rb
@@ -1,6 +1,15 @@
 class Yogurt < ActiveRecord::Base
 
-  validates_presence_of :flavor, :topping, :quantity
-  validates_numericality_of :quantity, greater_than: 0, less_than_or_equal_to: 12 # ounces
+  validates :flavor, presence: true
+  validates :flavor, length: {maximum: 30}
+
+  validates :topping, presence: true
+  validates :topping, length: {maximum: 30}
+  
+  validates :quantity, presence: true 
+  validates :quantity, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 12} # ounces
+  
+  # validates_presence_of :flavor, :topping, :quantity
+  # validates_numericality_of :quantity, greater_than: 0, less_than_or_equal_to: 12 # ounces
 
 end
--- spec/models/yogurt_spec.rb
+++ spec/models/yogurt_spec.rb
@@ -1,22 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Yogurt, :type => :model do
+  let(:yogurt) { Yogurt.new(flavor: 'chocolate', topping: 'oreos', quantity: 10.2) }
 
-  it "responds to flavor"
+  it "responds to flavor" do
+    expect(Yogurt.new).to respond_to(:flavor)
+  end
 
-  it "requires flavor"
+  it "requires flavor" do
+    yogurt.flavor = nil
+    expect(yogurt).to be_invalid
+  end
 
-  it "validates flavor charater count is less than 30 charaters"
+  it "validates flavor charater count is less than 30 charaters" do
+    yogurt.flavor = "nil" * 11
+    expect(yogurt).to be_invalid
+  end
 
-  it "requires topping"
+  it "requires topping" do
+    yogurt.topping = nil
+    expect(yogurt).to be_invalid
+  end
 
-  it "validates topping charater count is less than 30 charaters"
 
-  it "requires quantity"
+  it "validates topping charater count is less than 30 charaters" do
+    yogurt.topping = "nil" * 11
+    expect(yogurt).to be_invalid
+  end
 
-  it "validates quantity is greater than 0"
+  it "requires quantity"  do
+    yogurt.quantity = nil
+    expect(yogurt).to be_invalid
+  end
 
-  it "validates quantity is less than 12"
+  it "validates quantity is greater than 0" do
+    yogurt.quantity = -1.0
+    expect(yogurt).to be_invalid
+  end
+
+  it "validates quantity is less than 12" do
+    yogurt.quantity = 12.1
+    expect(yogurt).to be_invalid
+  end
 
   describe "#bite" do
 
